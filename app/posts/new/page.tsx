513fa2158840eb87bbcb9fa86c6b168ff2c7a979
Yiseul park <elane94@naver.com>
2024-09-26 15:00:12 +0900

    1st

---
+++ app/posts/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return
+  ;<div>
+    NewPostPage
+    <h1>New post page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos rem
+      assumenda libero laborum earum hic cum obcaecati a. Quaerat libero
+      possimus facilis nemo iure inventore quibusdam omnis exercitationem.
+      Vitae, laudantium!
+    </p>
+  </div>
+}
